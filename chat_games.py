ac96671fadac7a9147e1776419fd5e7c916e8ea3
Amanda Galvão <amandargarruda@icloud.com>
2025-10-30 10:16:25 -0300

    style: improve game description

--- chat_games.py
+++ chat_games.py
@@ -9,7 +9,9 @@ bot = telebot.TeleBot(CHAVE_API)
 def o1(mensagem):
     texto = """
     *God of War* \n
-    Depois de uma vida marcada pela guerra e pela tragédia, Kratos, o antigo deus da guerra da mitologia grega, busca redenção em uma nova terra: o reino dos deuses nórdicos. Vivendo isolado nas florestas da Escandinávia, ele tenta deixar para trás o passado violento e criar seu filho Atreus como um homem melhor do que ele foi. Quando a morte da esposa desencadeia uma jornada inesperada, pai e filho embarcam em uma perigosa viagem para cumprir seu último desejo: espalhar suas cinzas no ponto mais alto dos nove reinos. Ao longo do caminho, eles enfrentam monstros, deuses e segredos do passado, enquanto Kratos luta para controlar sua fúria e ensinar a Atreus o verdadeiro significado de força, honra e humanidade. Entre batalhas épicas e momentos de profunda emoção, God of War é uma história sobre paternidade, redenção e o eterno conflito entre o homem e o deus dentro de si.
+    Depois de uma vida marcada pela guerra e pela tragédia, Kratos, o antigo deus da guerra da mitologia grega, busca redenção em uma nova terra: o reino dos deuses nórdicos. Vivendo isolado nas florestas da Escandinávia, ele tenta deixar para trás o passado violento e criar seu filho Atreus como um homem melhor do que ele foi. 
+    Quando a morte da esposa desencadeia uma jornada inesperada, pai e filho embarcam em uma perigosa viagem para cumprir seu último desejo: espalhar suas cinzas no ponto mais alto dos nove reinos. Ao longo do caminho, eles enfrentam monstros, deuses e segredos do passado, enquanto Kratos luta para controlar sua fúria e ensinar a Atreus o verdadeiro significado de força, honra e humanidade. 
+    Entre batalhas épicas e momentos de profunda emoção, God of War é uma história sobre paternidade, redenção e o eterno conflito entre o homem e o deus dentro de si.
     """
     bot.send_message(mensagem.chat.id, texto)
 
@@ -17,7 +19,9 @@ def o1(mensagem):
 def o2(mensagem):
     texto = """
     *Red Dead Redemption 2* \n
-    No ocaso do Velho Oeste, o fora-da-lei Arthur Morgan e os membros da gangue Van der Linde lutam para sobreviver em um mundo que está mudando rapidamente. Após um assalto fracassado na cidade de Blackwater, o grupo é forçado a fugir, caçado por agentes federais e caçadores de recompensas. Enquanto a lealdade à gangue e ao carismático líder Dutch van der Linde começa a ser testada, Arthur enfrenta um dilema moral: seguir cegamente os ideais do bando ou buscar sua própria redenção. Em meio a paisagens deslumbrantes, tiroteios brutais e escolhas difíceis, ele descobre que cada decisão tem um preço — e que até mesmo os mais duros fora-da-lei não podem escapar de suas consequências. Red Dead Redemption 2 é uma jornada épica sobre honra, lealdade e a luta por um lugar em um mundo que está deixando os cowboys para trás.
+    No ocaso do Velho Oeste, o fora-da-lei Arthur Morgan e os membros da gangue Van der Linde lutam para sobreviver em um mundo que está mudando rapidamente. Após um assalto fracassado na cidade de Blackwater, o grupo é forçado a fugir, caçado por agentes federais e caçadores de recompensas. 
+    Enquanto a lealdade à gangue e ao carismático líder Dutch van der Linde começa a ser testada, Arthur enfrenta um dilema moral: seguir cegamente os ideais do bando ou buscar sua própria redenção. Em meio a paisagens deslumbrantes, tiroteios brutais e escolhas difíceis, ele descobre que cada decisão tem um preço — e que até mesmo os mais duros fora-da-lei não podem escapar de suas consequências. 
+    Red Dead Redemption 2 é uma jornada épica sobre honra, lealdade e a luta por um lugar em um mundo que está deixando os cowboys para trás.
     """
     bot.send_message(mensagem.chat.id, texto)
 
